6302559fdb1f0e88787ea4d3874fa7f93f0ec637
LeonardoOfc <ingenieria@silverlight.com.co>
2021-08-02 15:12:04 -0500

    Diseño sidebar y topbar por defecto

---
+++ html/components/sidebar.php
@@ -0,0 +1,82 @@
+
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-book"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Bibliiotek</div>
+      </a>
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="dashboard.php">
+          <i class="fas fa-fw fa-house-user"></i>
+          <span>Inicio</span>
+        </a>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Comercial
+      </div>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
+          aria-controls="collapseTwo">
+          <i class="fas fa-fw fa-book-reader"></i>
+          <span>Clientes</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledy="headingTwo" data-parient="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">            
+            <h6 class="collapse-header">Instituciones</h6>
+            <a class="collapse-item" href="bibliotecas.php">Bibliotecas</a>
+            <a class="collapse-item" href="librerias.php">Librerias</a>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true"
+          aria-controls="collapseUtilities">
+          <i class="fas fa-fw fa-users-cog"></i>
+          <span>Personas</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledy="headingTwo" data-parient="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">            
+            <h6 class="collapse-header">Afiliados</h6>
+            <a class="collapse-item" href="usuarios.php">Usuarios</a>
+            <a class="collapse-item" href="perfiles.php">Perfiles</a>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
+          aria-controls="collapsePages">
+          <i class="fas fa-fw fa-book"></i>
+          <span>Libros</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledy="headingTwo" data-parient="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">            
+            <h6 class="collapse-header">Inventario</h6>
+            <a class="collapse-item" href="usuarios.php">Libros</a>
+            <a class="collapse-item" href="perfiles.php">Cargue Masivo</a>
+          </div>
+        </div>
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Utilidades
+      </div>
+      <li class="nav-item">
+        <a class="nav-link" href="informes.php">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Informes</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="ajustes.php">
+          <i class="fas fa-fw fa-cogs"></i>
+          <span>Ajustes</span>
+        </a>
+      </li>
+      <hr class="sidebar-divider d-none d-md-block">
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+    </ul>
---
+++ html/components/topbar.php
@@ -0,0 +1,171 @@
+<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+  <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+    <i class="fa fa-bars"></i>
+  </button>
+  <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+    <div class="input-group">
+      <input type="text" class="form-control bg-light border-0 small" placeholder="Buscar..." aria-label="Buscar" 
+        aria-describedby="basic-addon2">
+      <div class="input-group-append">
+        <button class="btn btn-primary" type="button">
+          <i class="fas fa-search fa-sm"></i>
+        </button>
+      </div>
+    </div>
+  </form>
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item dropdown no-arrow d-sm-none">
+      <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" 
+        aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-search fa-fw"></i>
+      </a>
+      <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledy="searchDropdown">
+        <form class="form-inline mr-auto w-100 navbar-search">
+          <div class="input-group">
+            <input type="text" class="form-control bg-light border-0 small" placeholder="Buscar..." aria-label="Buscar"
+              aria-describedy="basic-addon2">
+            <div class="input-group-append">
+              <button class="btn btn-primary" type="button">
+                <i class="fas fa-search fa-sm"></i>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </li>
+    <li class="nav-item dropdown no-arrow mx-1">
+      <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" 
+        aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-bell fa-fw"></i>
+        <span class="badge badge-danger badge-counter">3+</span>
+      </a>
+      <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" 
+        aria-labelledy="alertsDropdown">
+        <h6 class="dropdown-header">
+          Notificaciones
+        </h6>
+        <a class="dropdown-item d-flex align-items-center" href="#">
+          <div class="mr-3">
+            <div class="icon-circle bg-primary">
+              <i class="fas fa-file-alt text-white"></i>
+            </div>
+          </div>
+          <div>
+            <div class="small text-gray-500">Agosto 5 - 2021</div>
+            <span class="font-weight-bold">Reporte del mes de Junio listo</span>
+          </div>
+        </a>
+        <a class="dropdown-item d-flex align-items-center" href="#">
+          <div class="mr-3">
+            <div class="icon-circle bg-success">
+              <i class="fas fa-donate text-white"></i>
+            </div>
+          </div>
+          <div>
+            <div class="small text-gray-500">Agosto 3 - 2021</div>
+            Ganancias del mes de Junio en tu cuenta
+          </div>
+        </a>
+        <a class="dropdown-item d-flex align-items-center" href="#">
+          <div class="mr-3">
+            <div class="icon-circle bg-warning">
+              <i class="fas fa-exclamation-triangle text-white"></i>
+            </div>
+          </div>
+          <div>
+            <div class="small text-gray-500">Agosto 1 - 2021</div>
+            La Biblioteca colegial ha sido desvinculada
+          </div>
+        </a>
+        <a class="dropdown-item text-center small text-gray-500" href="#">Mostrar todas las notificaciones</a>
+      </div>
+    </li>
+    <li class="nav-item dropdown no-arrow mx-1">
+      <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" 
+        aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-envelope fa-fw"></i>
+        <span class="badge badge-danger badge-counter">7</span>
+      </a>
+      <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" 
+        aria-labelledy="messagesDropdown">
+        <h6 class="dropdown-header">
+          Mensajes
+        </h6>
+        <a class="dropdown-item d-flex align-items-center" href="#">
+          <div class="dropdown-list-image mr-3">
+            <img class="rounded-circle" src="../../assets/images/profile-pictures/undraw_profile_1.svg" alt="...">
+            <div class="status-indicator bg-success"></div>
+          </div>
+          <div class="font-weight-bold">
+            <div class="text-truncate">Hola, estoy teniendo un problema con mi cuenta desde hace varios dias,
+              podrias ayudarme, por favor.</div>
+            <div class="small text-gray-500">Laura Maturana · 2h</div>
+          </div>
+        </a>
+        <a class="dropdown-item d-flex align-items-center" href="#">
+          <div class="dropdown-list-image mr-3">
+            <img class="rounded-circle" src="../../assets/images/profile-pictures/undraw_profile_2.svg" alt="...">
+            <div class="status-indicator"></div>
+          </div>
+          <div class="font-weight-bold">
+            <div class="text-truncate">Hola, estoy teniendo un problema con mi cuenta desde hace varios dias,
+              podrias ayudarme, por favor.</div>
+            <div class="small text-gray-500">Laura Maturana · 2h</div>
+          </div>
+        </a>
+        <a class="dropdown-item d-flex align-items-center" href="#">
+          <div class="dropdown-list-image mr-3">
+            <img class="rounded-circle" src="../../assets/images/profile-pictures/undraw_profile_3.svg" alt="...">
+            <div class="status-indicator bg-warning"></div>
+          </div>
+          <div class="font-weight-bold">
+            <div class="text-truncate">Hola, estoy teniendo un problema con mi cuenta desde hace varios dias,
+              podrias ayudarme, por favor.</div>
+            <div class="small text-gray-500">Laura Maturana · 2h</div>
+          </div>
+        </a>
+        <a class="dropdown-item d-flex align-items-center" href="#">
+          <div class="dropdown-list-image mr-3">
+            <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="...">
+            <div class="status-indicator bg-success"></div>
+          </div>
+          <div class="font-weight-bold">
+            <div class="text-truncate">Hola, estoy teniendo un problema con mi cuenta desde hace varios dias,
+              podrias ayudarme, por favor.</div>
+            <div class="small text-gray-500">Laura Maturana · 2h</div>
+          </div>
+        </a>
+        <a class="dropdown-item text-center small text-gray-500" href="#">Ver todos los mensajes</a>
+      </div>
+    </li>
+
+    <div class="topbar-divider d-none d-sm-block"></div>
+
+    <li class="nav-item dropdown no arrow">
+      <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" 
+        aria-haspopup="true" aria-expanded="false">
+        <span class="mr-2 d-none d-lg-inline text-gray-600 small">Leonardo Diaz</span>
+        <img class="img-profile rounded-circle" src="../../assets/images/profile-pictures/undraw_profile.svg">
+      </a>
+      <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledy="userDropdown">
+        <a class="dropdown-item" href="#">
+          <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+          Perfil
+        </a>
+        <a class="dropdown-item" href="#">
+          <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+          Ajustes
+        </a>
+        <a class="dropdown-item" href="#">
+          <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+          Log de Actividades
+        </a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+          Cerrar Sesion
+        </a>
+      </div>
+    </li>
+  </ul>
+</nav>
